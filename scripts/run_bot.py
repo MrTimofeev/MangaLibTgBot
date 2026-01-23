c4996ec5306183d6326a3b96163da1c0d4b1f350
MrTimofeev <nemo.capitan.26ru@mail.ru>
2026-01-23 17:12:19 +0300

    Добавлен скрипт для запуска бота

--- scripts/run_bot.py
+++ scripts/run_bot.py
@@ -1,1 +1,9 @@
-from 
+from mangabot.main import main
+import asyncio
+
+if __name__ == "__main__":
+    # Запускаем основную функцию
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit) as e:
+        print(f"Ошибка при завершении: {e}")
